25f20d3f07f4e3ef0f742907eaf620c5aeadfcb3
Carsten Kremser <github@carstenkremser.de>
2024-03-28 10:34:35 +0100

    Remove unused import

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();

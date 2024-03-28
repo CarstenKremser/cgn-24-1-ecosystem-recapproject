17bcbfe634ed0db056567f67451a6e1ef2179925
Carsten Kremser <github@carstenkremser.de>
2024-03-28 15:36:36 +0100

    Implement some functionality in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         OrderRepo orderRepo = new OrderMapRepo();
         ProductRepo productRepo = new ProductRepo();
         ShopService shopService = new ShopService(productRepo,orderRepo);
+
+        Product[] products = new Product[]{
+                new Product("a", "Apfel"),
+                new Product("b", "Birne"),
+                new Product("c", "Chili"),
+                new Product("d", "Dattel"),
+                new Product("e", "Erdnuss")
+        };
+        for (Product product:products) {
+            productRepo.addProduct(product);
+        }
+        try {
+            shopService.addOrder(List.of("a","b"));
+            shopService.addOrder(List.of("b","c"));
+            shopService.addOrder(List.of("c","d"));
+            shopService.addOrder(List.of("d","e"));
+            shopService.addOrder(List.of("a","e","b","d","c"));
+        } catch (ProductDoesNotExistException e) {
+            System.out.println("Exception: " + e);
+        }
+        System.out.println(shopService.getAllOrdersWithStatus(OrderStatus.PROCESSING));
+
     }
 }

1b4299288b9de4b070ffb2b90d8344c10a3be71b
Carsten Kremser <github@carstenkremser.de>
2024-03-29 17:45:23 +0100

    Prepare demo data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,8 +13,14 @@ public class Main {
                 new Product("b", "Birne"),
                 new Product("c", "Chili"),
                 new Product("d", "Dattel"),
-                new Product("e", "Erdnuss")
+                new Product("e", "Erdnuss"),
+                new Product("1", "Aprilia"),
+                new Product("2", "Bimota"),
+                new Product("3", "Cagiva"),
+                new Product("4", "Ducati"),
+                new Product("5", "Energica")
         };
+        productRepo.removeProduct("1");
         for (Product product:products) {
             productRepo.addProduct(product);
         }
